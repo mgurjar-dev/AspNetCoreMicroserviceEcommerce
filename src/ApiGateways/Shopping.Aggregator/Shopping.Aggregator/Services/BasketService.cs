b34fdb85a202083f9d9de7b4911a36e27b5b9c4c
Manish Gurjar <manishgurjar5@gmail.com>
2021-09-29 23:36:47 +0530

    Aggregator gateway

---
+++ src/ApiGateways/Shopping.Aggregator/Shopping.Aggregator/Services/BasketService.cs
@@ -0,0 +1,28 @@
+﻿using Shopping.Aggregator.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Shopping.Aggregator.Extensions;
+using System.Text.Json;
+using Shopping.Aggregator.Services;
+
+namespace Shopping.Aggregator.Services
+{
+    public class BasketService : IBasketService
+    {
+        private readonly HttpClient httpClient;
+
+        public BasketService(HttpClient httpClient)
+        {
+            this.httpClient = httpClient;
+        }
+
+        public async Task<BasketModel> GetBasket(string userName)
+        {
+            var response = await httpClient.GetAsync($"/api/basket/{userName}");
+            return await response.ReadContentAs<BasketModel>();
+        }
+    }
+}
